1313489b99890cf0b9692fb3db066afa7de73bdb
Kaizer Sallong Balite <146916590+KaizerBalite@users.noreply.github.com>
2023-10-13 11:24:25 +0800

    feat: Activity 8 in DSA
    Array in javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+//Activity 8
+
+const favSingers = ["Kendrick Lamar", "Drake", "Mac Miller"];
+const favNumbers = [9, 6, 3, 18];
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log(favSingers);
+console.log(favNumbers);
+
+console.log(favSingers[0]);
+console.log(favSingers[1]);
+console.log(favSingers[2]);
+console.log(favNumbers[1]);
+console.log(mixedArr[3]);
